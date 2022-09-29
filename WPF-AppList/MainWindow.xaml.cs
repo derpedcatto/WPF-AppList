2ed48e6a76c4fcd884acea6ad4c08bda8f1aa7cc
Alexander Vannovskiy <sashavannovski@gmail.com>
2022-09-29 16:16:45 +0300

    Add project files.

---
+++ WPF-AppList/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace WPF_AppList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ButtonContainers_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Not done yet!");
+        }
+
+        private void ButtonMinesweeper_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Not done yet!");
+        }
+
+        private void ButtonGDI_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Not done yet!");
+        }
+    }
+}
