3b7019f59fdd6f9f9f89ef7e3a187bf6469c7df4
Alexander Vannovskiy <sashavannovski@gmail.com>
2022-10-08 11:53:20 +0300

    new form added

---
+++ WPF-AppList/5-4_Homework/FormRegistrationWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_AppList._5_4_Homework
+{
+    /// <summary>
+    /// Interaction logic for FormRegistrationWindow.xaml
+    /// </summary>
+    public partial class FormRegistrationWindow : Window
+    {
+        public FormRegistrationWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
